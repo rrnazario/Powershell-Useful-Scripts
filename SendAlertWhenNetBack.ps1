14d60e8128875bd30510a137a6002edca8f5d587
Rogerson Reis Nazario <M6024365@mgmt.tlrg.com>
2020-08-20 15:11:20 -0300

    Send message when net is back

---
+++ SendAlertWhenNetBack.ps1
@@ -0,0 +1,23 @@
+while (!(Test-Connection 8.8.8.8 -Count 1 -Quiet -ErrorAction SilentlyContinue))
+{
+    "Nothing"
+    Start-Sleep -s 30
+}
+
+#Send Telegram Message
+$telegramToken = "TELEGRAM_BOT_TOKEN"
+$payload = @{
+    "chat_id" = "_YOUR_CHAT_ID";
+    "text" = "Internet is back!";
+}
+
+
+while ($true){
+    Invoke-WebRequest `
+                -Uri("https://api.telegram.org/bot{0}/sendMessage" -f $token) `
+                -Method Post `
+                -ContentType "application/json;charset=utf-8" `
+                -Body (ConvertTo-Json -Compress -InputObject $payload)
+
+    Start-Sleep -s 10
+}
